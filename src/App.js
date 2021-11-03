a93a338e6205f55b3480ae482f05f79fcc4e427d
Ade <shennaike2386@hotmail.com>
2021-11-03 01:05:49 -0400

    finished comments js, posts js, and likesection js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
-
+const [posts,setPosts] = useState(dummyData)
   const likePost = postId => {
     /*
       This function serves the purpose of increasing the number of likes by one, of the post with a given id.
@@ -30,6 +30,10 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    const newPosts = posts.map(pst => {
+      return pst.id === postId ? { ...pst, likes: pst.likes + 1} : pst 
+    }) 
+    setPosts(newPosts)
   };
 
   return (
